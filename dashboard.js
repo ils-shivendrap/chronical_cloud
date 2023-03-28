16513b7e4a26a77b44c5854efaffdf841d3a2041
Shivendra Patel <sh.patel@thesynapses.com>
2023-03-28 10:24:17 +0530

    dashboard work

---
+++ dashboard.js
@@ -0,0 +1,46 @@
+const barChart = document.getElementById('barChart');
+
+  new Chart(barChart, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+
+
+  const pieChart = document.getElementById('pieChart');
+  new Chart(pieChart, {
+    type: 'pie',
+    data: {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      }
+  });
+  
