6ad93d30cab71f94c2dd0618e98ae6c0bc028e6a
Shivendra Patel <sh.patel@thesynapses.com>
2023-03-29 08:58:19 +0530

    dashboard completed

--- dashboard.js
+++ dashboard.js
@@ -1,46 +1,200 @@
 const barChart = document.getElementById('barChart');
 
-  new Chart(barChart, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
+  // new Chart(barChart, {
+  //   type: 'bar',
+  //   data: {
+  //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  //     datasets: [{
+  //       label: '# of Votes',
+  //       data: [12, 19, 3, 5, 2, 3],
+  //       borderWidth: 1
+  //     }]
+  //   },
+  //   options: {
+  //     scales: {
+  //       y: {
+  //         beginAtZero: true
+  //       }
+  //     }
+  //   }
+  // });
+
+//Bar graph JS
+var xValues = ["School_Name_01", "School_Name_02", "School_Name_03", "School_Name_04"];
+  
+new Chart("barChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      label: "Active",
+      backgroundColor: "#F9AC32",
+      data: [150, 200, 350, 250],
+      barThickness:20
+    },{
+        label:"Inactive",
+        backgroundColor: "#EEEEEE",
+        data: [200, 250, 100, 50],
+        barThickness:20
+    }]
+  },
+  options: {
+    plugins: {
+      datalabels:{
+        display: false
+      },
+      legend: {
+        display: true,
+        position: 'right',
+        align: 'start',
+        labels: {
+            usePointStyle	: true,
+            pointStyle: "circle",
+            fontColor: '#333',
+            boxWidth: 6,
+            boxHeight: 6,
+            borderRadius: "50",
+            
+        },
+        
     },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+      title: {
+        display: false,
+      },
+    },
+    
+    scales: {
+      x:  {
+        stacked:true,
+        grid: {
+          display: false
+        },
+      },
+  
+      y: {
+        stacked:true,
+        grid:{
+          drawBorder: false
+        },
+        min: 0,
+        max: 400,
+        ticks: {
+          // forces step size to be 50 units
+          stepSize: 100
         }
-      }
-    }
-  });
+      },
+    },
+    responsive: true,
+    aspectRatio: 2
+  },
+});
 
 
 
 
-  const pieChart = document.getElementById('pieChart');
-  new Chart(pieChart, {
-    type: 'pie',
-    data: {
-        labels: [
-          'Red',
-          'Blue',
-          'Yellow'
-        ],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
-        }]
-      }
-  });
+const pieChart = document.getElementById('pieChart');
+
+//pie chart
+var pieXValues = ["Multimedia", "Audio Notes", "Notes","Free Space"];
+var pieYValues = [ 85, 115, 55, 245];
+var barColors = ["#286BCB", "#7DB0F7", "#CCCCCC", "#386CB5"];
+
+//custom data labels plugin block
+
+
+new Chart("pieChart", {
+  type: "pie",
+  data: {
+    labels: pieXValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: pieYValues,
+      borderWidth: 6
+    }]
+  },
+  options: {
+    rotation: 45,
+    plugins: {
+      
+      labels:{
+        render: (ctx)=>{
+          return ctx.value + " mb ";
+        },
+        position:"outside",
+        fontColor: barColors
+      },
+      datalabels:{
+        formatter: (value, ctx) => {
+          let sum = 0;
+          let dataArr = ctx.chart.data.datasets[0].data;
+          dataArr.map(data => {
+            sum += data;
+          });
+          let percentage = (value*100 / sum).toFixed(0)+"%";
+          return percentage;
+        },
+        labels: {
+          title: {
+            font: {
+              weight: 'bold'
+            }
+          },
+        },
+        color: "#FFFFFF"
+      },
+      legend: {
+        display: true,
+        position: 'left',
+        align: 'end',
+        labels: {
+            usePointStyle	: true,
+            pointStyle: "circle",
+            fontColor: '#333',
+            boxWidth: 10,
+            boxHeight: 10,
+            borderRadius: "50",
+        }
+    },
+      title: {
+        display: false,
+        beginAtZero: true,
+      },
+      type: 'linear'
+    },
+    responsive: true,
+    aspectRatio: 2
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
   
